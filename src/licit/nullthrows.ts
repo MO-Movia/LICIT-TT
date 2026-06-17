d672834e731e78fad4ff533aae0ad0545d1ac30f
Sarath <sarath.chandran@modusoperandi.com>
2026-06-17 12:35:49 +0530

    Replace the nullthrows dependency with a local nullthrows utility function.

---
+++ src/licit/nullthrows.ts
@@ -0,0 +1,16 @@
+/**
+ * @license MIT
+ * @copyright Copyright 2026 Modus Operandi Inc. All Rights Reserved.
+ */
+
+/**
+ * Throws if value is null or undefined, otherwise returns value.
+ */
+export default function nullthrows<T>(value?: T | null, message?: string): T {
+  if (value != null) {
+    return value;
+  }
+  throw new Error(
+    message !== undefined ? message : `Got unexpected ${String(value)}`
+  );
+}
